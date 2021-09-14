c93b13e8db3ae18885d0156ea380839bc488b861
Виктория <cool.goodvika2077@ya.ru>
2021-09-14 12:22:10 +0300

    Mattress_2

--- src/main/java/ru/neo/Main.java
+++ src/main/java/ru/neo/Main.java
@@ -3,15 +3,28 @@ package ru.neo;
 public class Main {
 
     public static void main(String[] args) {
-        Mattress matr1 = new Mattress(200,180, 20);
-        Mattress matr2 = new Mattress(210,200, 30);
+        Mattress matr1 = new Mattress(200,200, 20);
+        Mattress matr2 = new Mattress(200,220, 30);
         Mattress matr3 = new Mattress(130, 140, 15);
 
         matr1.manufacturerName("Matr1");
         matr2.manufacturerName("Matr2");
+        System.out.println();
 
         matr1.setDensity("high density");
         matr2.setDensity("low density");
         matr3.setDensity("super density");
+        System.out.println();
+
+        matr2.setSheetSize(200,220, 30);
+        matr3.setSheetSize(130, 140, 15);
+        System.out.println();
+
+        System.out.print("Matr1 ");
+        matr1.setStandBedSheets(200,200);
+        System.out.print("Matr2 ");
+        matr2.setStandBedSheets(220,220);
+        System.out.print("Matr3 ");
+        matr3.setStandBedSheets(130, 140);
     }
 }
--- src/main/java/ru/neo/Mattress.java
+++ src/main/java/ru/neo/Mattress.java
@@ -4,9 +4,6 @@ public class Mattress {
     int length;
     int width;
     int height;
-    String density;
-
-    String significance;
 
     public Mattress(int length, int width, int height) {
         this.length = length;
@@ -14,28 +11,51 @@ public class Mattress {
         this.height = height;
     }
 
-    void manufacturerName (String name) {
-        if (width > 180 || density == "high density") {
-            System.out.println(name + " двухместный");
+    public void manufacturerName (String name) {
+        if (width > 200 || height > 25) {
+            System.out.println(name + " is double");
         }
         else
-            System.out.println(name + " полуторка");
+            System.out.println(name + " is one and a half ");
     }
 
     void setDensity (String density) {
         switch (density) {
             case "high density":
-                significance = "high density";
-                System.out.println("Suitable for scoliosis for " + density);
+                //significance = "high density";
+                System.out.println("Suitable for scoliosis for - " + density);
                 break;
             case "low density":
-                significance = "low density";
-                System.out.println("Требуется консультация специалиста при" + density);
+                //significance = "low density";
+                System.out.println("A specialist's consultation is required with the parameter = " + density);
                 break;
             default:
-                System.out.println("Уточните корректность плотности при параметре " + density);
+                System.out.println("Specify the correctness of the density for the parameter = " + density);
         }
     }
 
-//    void
+    public void setSheetSize (int length, int width, int height) {
+        if (length > 190 & width > 190 & height > 25 ) {
+            int shLength = length + 5;
+            int shWidth = width + 5;
+            int shHeight = height + 7;
+            System.out.println("Length/width/height of the sheet with an elastic band: " + shLength + "/" + shWidth + "/" + shHeight);
+        }
+        else {
+            System.out.println("The size of the sheet corresponds to the size of the mattress");
+        }
+    }
+
+    public void setStandBedSheets (int length, int width) {
+        if (length == 200 & width == 200) {
+            System.out.println("Euro sheets");
+            return;
+        }
+        if (length == 220 & width == 220) {
+            System.out.println("Super-euro sheets");
+        }
+        else {
+            System.out.println("Custom sheet");
+        }
+    }
 }
